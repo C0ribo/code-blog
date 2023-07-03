5ed85d76604d8b8abf887e420aa8e4882e89ee76
EunKyo Chae <dmsry1022@gmail.com>
2023-07-04 03:19:59 +0900

    blog name

--- blog-config.js
+++ blog-config.js
@@ -1,14 +1,12 @@
 module.exports = {
   title: "cori.blog",
-  description: "Hello :) I'm Hudi who developed gatsby-starter-hoodie theme.",
+  description: "개발자 취준생의 기록 블로그",
   author: "Cori",
-  siteUrl: "https://devhudi.github.io/gatsby-starter-hoodie/",
+  siteUrl: "https://cori-blog.netlify.app/",
   links: {
-    github: "https://github.com/devHudi",
-    linkedIn: "https://linkedin.com",
-    facebook: "https://www.facebook.com",
-    instagram: "https://www.instagram.com",
-    email: "mailto:devhudi@gmail.com",
+    github: "https://github.com/C0ribo",
+    etc : "https://c0ribo-fedev-blog.netlify.app/",
+    email: "mailto:dmsry1022@gmail.com",
   },
   utterances: {
     repo: "devHudi/gatsby-starter-hoodie",
